d886bd2c33afe357e48adcd7fdd7e75629e5bfb9
Clo3n <ricards.tomsons@gmail.com>
2024-12-29 19:16:22 +0200

    datubaze

---
+++ database/izmaksas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `izmaksas` (
+	`izmaksas_id` INT NOT NULL,
+	`user_id` INT NOT NULL,
+	`sum` DOUBLE NULL DEFAULT NULL,
+	`date` DATE NULL,
+	PRIMARY KEY (`izmaksas_id`),
+	CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE NO ACTION ON DELETE NO ACTION
+);
---
+++ database/user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+	`user_id` INT NOT NULL AUTO_INCREMENT,
+	`username` TEXT NULL,
+	`email` TEXT NULL,
+	`password` TEXT NULL,
+	PRIMARY KEY (`user_id`)
+);
