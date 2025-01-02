4ceb26ab0c53956cdc30afdd265315d7f7f05ebc
Clo3n <ricards.tomsons@gmail.com>
2025-01-02 13:02:48 +0200

    izmaksas Alter

--- database/izmaksas.sql
+++ database/izmaksas.sql
@@ -2,7 +2,8 @@ CREATE TABLE `izmaksas` (
 	`izmaksas_id` INT NOT NULL,
 	`user_id` INT NOT NULL,
 	`sum` DOUBLE NULL DEFAULT NULL,
-	`date` DATE NULL,
+	`date` TEXT NULL,
+	`category` TEXT NULL,
 	PRIMARY KEY (`izmaksas_id`),
 	CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
