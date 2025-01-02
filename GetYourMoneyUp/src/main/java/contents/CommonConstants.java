17476e4c493584ae635965ffee7728953d334478
deividsagold <deividsgold@gmail.com>
2025-01-02 19:04:25 +0200

    added db

--- GetYourMoneyUp/src/main/java/contents/CommonConstants.java
+++ GetYourMoneyUp/src/main/java/contents/CommonConstants.java
@@ -7,8 +7,8 @@ public class CommonConstants {
     public static final Color SECONDARY_COLOR = Color.DARK_GRAY;
     public static final Color TEXT_COLOR = Color.BLACK;
 
-    public static final String DB_URL ="" ;//add db url
+    public static final String DB_URL ="jdbc:mysql://localhost:3306/gymu_project";//add db url
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "password";
+    public static final String DB_PASSWORD = "";
     public static final String DB_USERS_TABLE_NAME = "USERS";
 }
