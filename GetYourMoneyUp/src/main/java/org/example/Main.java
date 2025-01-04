591293af47f446b80e796921a0cf9e435b56b57a
Clo3n <ricards.tomsons@gmail.com>
2025-01-04 15:20:06 +0200

    database connection done

--- GetYourMoneyUp/src/main/java/org/example/Main.java
+++ GetYourMoneyUp/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String url = "jdbc:mysql://localhost:3306/gymu_project";
+        String user = "root";
+        String password = "";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
